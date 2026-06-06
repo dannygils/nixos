3b3beb518d0238a981c6183a8b11af83b7aaeee6
Dan <danielgilfeather@gmail.com>
2026-06-06 08:58:43 -0400

    fixing driver stuff so plex plays better

--- nvidia.nix
+++ nvidia.nix
@@ -23,4 +23,17 @@
   ############################
   hardware.graphics.enable      = true;
   hardware.graphics.enable32Bit = true;
+  hardware.graphics.extraPackages = with pkgs; [
+    nvidia-vaapi-driver   # VA-API -> NVDEC bridge for hardware video decode
+  ];
+
+  ############################
+  # Environment: VA-API + EGL
+  ############################
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME         = "nvidia";
+    NVD_BACKEND               = "direct";
+    GBM_BACKEND               = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  };
 }
